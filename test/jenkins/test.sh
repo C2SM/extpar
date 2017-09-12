67bcecb43a01cd7ffb51d039c46796e15f4e411b
Katherine Osterried <katherine.osterried@env.ethz.ch>
2017-09-12 15:12:47 +0200

    Make Jenkins check testsuite output

--- test/jenkins/test.sh
+++ test/jenkins/test.sh
@@ -11,3 +11,15 @@ cd data
 cd ..
 
 ./submit.daint.sh
+
+# echo output to stdout
+test -f testsuite.out || exitError 1261 ${LINENO} "output file testsuite.out does not exist"
+echo "=== testsuite.out BEGIN ==="
+cat testsuite.out | /bin/sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g"
+echo "=== testsuite.out END ==="
+
+# check result of testsuite
+grep RESULT testsuite.out | egrep 'FAIL|CRASH' > /dev/null
+if [ $? -eq 0 ] ; then
+  exitError 1271 ${LINENO} "testsuite did not complete successfully"
+fi
