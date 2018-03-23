709ba2e9ea81ef26a0da4556fc503ee27495572b
Juergen Helmert <juergen.helmert@dwd.de>
2018-03-23 11:03:34 +0000

    Fix argument mismatch in extpar_topo_to_buffer.f90

--- src/extpar_topo_to_buffer.f90
+++ src/extpar_topo_to_buffer.f90
@@ -427,14 +427,12 @@ PROGRAM extpar_topo_to_buffer
   !--------------------------------------------------------------------------------------------------------
  IF (igrid_type == igrid_icon) THEN ! ICON GRID
    IF (lsso_param) THEN
-     IF (lscale_separation) THEN
      PRINT *,'CALL agg_topo_data_to_target_grid with SSO'
      CALL agg_topo_data_to_target_grid_icon(topo_tiles_grid,  &
        &                                topo_grid,        &
        &                                tg,               &
        &                                topo_files,       &
        &                                lsso_param,       &
-       &                                lscale_separation,&
        &                                lsubtract_mean_slope, &
        &                                lfilter_oro,      &
        &                                lfilter_topo,      &
@@ -455,20 +453,49 @@ PROGRAM extpar_topo_to_buffer
        &                                no_raw_data_pixel,&
        &                                theta_topo,&
        &                                aniso_topo,&
-       &                                slope_topo,       &
-       &                                raw_data_orography_path=raw_data_orography_path) !_br 17.09.14)
-     ENDIF
+       &                                slope_topo        )
    ELSE
-     IF (lscale_separation) THEN
-       CALL agg_topo_data_to_target_grid_icon(topo_tiles_grid, &
+     PRINT *,'CALL agg_topo_data_to_target_grid without SSO'
+     CALL agg_topo_data_to_target_grid_icon(topo_tiles_grid,  &
        &                                topo_grid,        &
        &                                tg,               &
        &                                topo_files,       &
        &                                lsso_param,       &
-!< *mes
-       &                                lscale_separation,&
-!> *mes
 !roa>
+       &                                lsubtract_mean_slope, &
+       &                                lfilter_oro,      &
+       &                                lfilter_topo,      &
+       &                                ilow_pass_oro,    &
+       &                                numfilt_oro,      &
+       &                                eps_filter,       &
+       &                                ifill_valley,     &
+       &                                rfill_valley,     &
+       &                                ilow_pass_xso,    &
+       &                                numfilt_xso,      &
+       &                                lxso_first,       &
+       &                                rxso_mask,        & 
+!roa<
+       &                                hh_topo,         &
+       &                                stdh_topo,       &
+       &                                fr_land_topo,    &
+       &                                z0_topo,          &
+       &                                no_raw_data_pixel )
+ 
+       ENDIF
+
+    ELSE  ! COSMO/GME GRID
+   IF (lsso_param) THEN
+     IF (lscale_separation) THEN
+       PRINT *,'CALL agg_topo_data_to_target_grid_cosmo with SSO'
+       CALL agg_topo_data_to_target_grid_cosmo(topo_tiles_grid, &
+       &                                topo_grid,        &
+       &                                tg,               &
+       &                                topo_files,       &
+       &                                lsso_param,       &
+!< *mes                                                                                                                                                                                                                                   
+       &                                lscale_separation,&
+!> *mes                                                                                                                                                                                                                                   
+!roa>                                                                                                                                                                                                                                     
        &                                lfilter_oro,      &
        &                                ilow_pass_oro,    &
        &                                numfilt_oro,      &
@@ -479,28 +506,29 @@ PROGRAM extpar_topo_to_buffer
        &                                numfilt_xso,      &
        &                                lxso_first,       &
        &                                rxso_mask,        &
-!roa<
+!roa<                                                                                                                                                                                                                                     
        &                                hh_topo,          &
        &                                stdh_topo,        &
        &                                fr_land_topo,     &
        &                                z0_topo,          &
        &                                no_raw_data_pixel,&
-       &                                raw_data_orography_path=raw_data_orography_path,& !_br 17.09.14
-       &                                raw_data_scale_sep_orography_path=raw_data_scale_sep_orography_path,& !_br 17.09.14
+       &                                theta_topo,       &
+       &                                aniso_topo,       &
+       &                                slope_topo,       &
+!< *mes                                                                                                                                                                                                                                   
+       &                                raw_data_orography_path=raw_data_orography_path,& !_br 17.09.14                                                                                                                                   
+       &                                raw_data_scale_sep_orography_path=raw_data_scale_sep_orography_path,& !_br 17.09.14                                                                                                               
        &                                scale_sep_files = scale_sep_files)
-!
+!> *mes                                                                                                                                                                                                                                   
      ELSE
-     PRINT *,'CALL agg_topo_data_to_target_grid without SSO'
-     CALL agg_topo_data_to_target_grid_icon(topo_tiles_grid,  &
+       CALL agg_topo_data_to_target_grid_cosmo(topo_tiles_grid, &
        &                                topo_grid,        &
        &                                tg,               &
        &                                topo_files,       &
        &                                lsso_param,       &
        &                                lscale_separation,&
-!roa>
-       &                                lsubtract_mean_slope, &
+!roa>                                                                                                                                                                                                                                     
        &                                lfilter_oro,      &
-       &                                lfilter_topo,      &
        &                                ilow_pass_oro,    &
        &                                numfilt_oro,      &
        &                                eps_filter,       &
@@ -509,50 +537,17 @@ PROGRAM extpar_topo_to_buffer
        &                                ilow_pass_xso,    &
        &                                numfilt_xso,      &
        &                                lxso_first,       &
-       &                                rxso_mask,        & 
-!roa<
-       &                                hh_topo,         &
-       &                                stdh_topo,       &
-       &                                fr_land_topo,    &
-       &                                z0_topo,          &
-       &                                no_raw_data_pixel, &
-       &                                raw_data_orography_path=raw_data_orography_path) !_br 17.09.14)
-     ENDIF
- 
-  ENDIF
-
-    ELSE  ! COSMO/GME GRID
-   IF (lsso_param) THEN
-     IF (lscale_separation) THEN
-     PRINT *,'CALL agg_topo_data_to_target_grid with SSO'
-     CALL agg_topo_data_to_target_grid_cosmo(topo_tiles_grid,  &
-       &                                topo_grid,        &
-       &                                tg,               &
-       &                                topo_files,       &
-       &                                lsso_param,       &
-       &                                lscale_separation,&
-       &                                lsubtract_mean_slope, &
-       &                                lfilter_oro,      &
-       &                                lfilter_topo,      &
-       &                                ilow_pass_oro,    &
-       &                                numfilt_oro,      &
-       &                                eps_filter,       &
-       &                                ifill_valley,     &
-       &                                rfill_valley,     &
-       &                                ilow_pass_xso,    &
-       &                                numfilt_xso,      &
-       &                                lxso_first,       &
-       &                                rxso_mask,        & 
-!roa<
-       &                                hh_topo,         &
-       &                                stdh_topo,       &
-       &                                fr_land_topo,    &
+       &                                rxso_mask,        &
+!roa<                                                                                                                                                                                                                                     
+       &                                hh_topo,          &
+       &                                stdh_topo,        &
+       &                                fr_land_topo,     &
        &                                z0_topo,          &
        &                                no_raw_data_pixel,&
-       &                                theta_topo,&
-       &                                aniso_topo,&
+       &                                theta_topo,       &
+       &                                aniso_topo,       &
        &                                slope_topo,       &
-       &                                raw_data_orography_path=raw_data_orography_path) !_br 17.09.14)
+       &                                raw_data_orography_path=raw_data_orography_path) !_br 17.09.14)                                                                                                                                   
      ENDIF
    ELSE
      IF (lscale_separation) THEN
@@ -561,10 +556,10 @@ PROGRAM extpar_topo_to_buffer
        &                                tg,               &
        &                                topo_files,       &
        &                                lsso_param,       &
-!< *mes
+!< *mes                                                                                                                                                                                                                                   
        &                                lscale_separation,&
-!> *mes
-!roa>
+!> *mes                                                                                                                                                                                                                                   
+!roa>                                                                                                                                                                                                                                     
        &                                lfilter_oro,      &
        &                                ilow_pass_oro,    &
        &                                numfilt_oro,      &
@@ -575,28 +570,26 @@ PROGRAM extpar_topo_to_buffer
        &                                numfilt_xso,      &
        &                                lxso_first,       &
        &                                rxso_mask,        &
-!roa<
+!roa<                                                                                                                                                                                                                                     
        &                                hh_topo,          &
        &                                stdh_topo,        &
        &                                fr_land_topo,     &
        &                                z0_topo,          &
        &                                no_raw_data_pixel,&
-       &                                raw_data_orography_path=raw_data_orography_path,& !_br 17.09.14
-       &                                raw_data_scale_sep_orography_path=raw_data_scale_sep_orography_path,& !_br 17.09.14
+       &                                raw_data_orography_path=raw_data_orography_path,& !_br 17.09.14                                                                                                                                   
+       &                                raw_data_scale_sep_orography_path=raw_data_scale_sep_orography_path,& !_br 17.09.14                                                                                                               
        &                                scale_sep_files = scale_sep_files)
-!
+!                                                                                                                                                                                                                                         
      ELSE
-     PRINT *,'CALL agg_topo_data_to_target_grid without SSO'
-     CALL agg_topo_data_to_target_grid_cosmo(topo_tiles_grid,  &
+       PRINT *,'CALL agg_topo_data_to_target_grid_cosmo without SSO'
+       CALL agg_topo_data_to_target_grid_cosmo(topo_tiles_grid, &
        &                                topo_grid,        &
        &                                tg,               &
        &                                topo_files,       &
        &                                lsso_param,       &
        &                                lscale_separation,&
-!roa>
-       &                                lsubtract_mean_slope, &
+!roa>                                                                                                                                                                                                                                     
        &                                lfilter_oro,      &
-       &                                lfilter_topo,      &
        &                                ilow_pass_oro,    &
        &                                numfilt_oro,      &
        &                                eps_filter,       &
@@ -605,17 +598,18 @@ PROGRAM extpar_topo_to_buffer
        &                                ilow_pass_xso,    &
        &                                numfilt_xso,      &
        &                                lxso_first,       &
-       &                                rxso_mask,        & 
-!roa<
+       &                                rxso_mask,        &
+!roa<                                                                                                                                                                                                                                     
        &                                hh_topo,         &
        &                                stdh_topo,       &
        &                                fr_land_topo,    &
        &                                z0_topo,          &
        &                                no_raw_data_pixel, &
-       &                                raw_data_orography_path=raw_data_orography_path) !_br 17.09.14)
+       &                                raw_data_orography_path=raw_data_orography_path) !_br 17.09.14)                                                                                                                                   
      ENDIF
- 
+
   ENDIF
+
 END IF !igrid_type
    
    ! if the target domain has a higher resolution of than the GLOBE data set (30'') some grid elements might not
