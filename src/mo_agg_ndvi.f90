96432046e354706ccd6445f7158e9db994fe5fd1
Luis Kornblueh <luis.kornblueh@mpimet.mpg.de>
2018-10-18 14:15:51 +0200

    Removed all remainders of GME

--- src/mo_agg_ndvi.f90
+++ src/mo_agg_ndvi.f90
@@ -39,8 +39,6 @@ MODULE mo_agg_ndvi
 
   USE mo_grid_structures, ONLY: igrid_icon
   USE mo_grid_structures, ONLY: igrid_cosmo
-  USE mo_grid_structures, ONLY: igrid_gme
-
 
   USE mo_search_ll_grid, ONLY: find_reg_lonlat_grid_element_index, &
     &                          find_rotated_lonlat_grid_element_index
@@ -80,12 +78,6 @@ PUBLIC :: agg_ndvi_data_to_target_grid
                                       no_raw_data_pixel
         
     USE mo_target_grid_data, ONLY: search_res !< resolution of ICON grid search index list
-
-    USE mo_gme_grid, ONLY: gme_grid
-    USE mo_gme_grid, ONLY: sync_diamond_edge
-    USE mo_gme_grid, ONLY: gme_real_field, gme_int_field
-    USE mo_gme_grid, ONLY: cp_buf2gme, cp_gme2buf
-
 
        ! USE structure which contains the definition of the COSMO grid
        USE  mo_cosmo_grid, ONLY: COSMO_grid !< structure which contains the definition of the COSMO 
@@ -314,25 +306,7 @@ END IF
 
     END DO data_rows
 
- print *,'MAXVAL of NDVI_sum is ', MAXVAL(ndvi_sum)
-
-     SELECT CASE(tg%igrid_type)
-     CASE(igrid_gme)  ! in GME grid the diamond edges need to be synrchonized
-
-       ! ndvi_sum
-       CALL cp_buf2gme(tg,gme_grid,ndvi_sum,gme_real_field)
-       CALL sync_diamond_edge(gme_grid, gme_real_field)
-       CALL cp_gme2buf(tg,gme_grid,gme_real_field,ndvi_sum)
-
-      ! no_raw_data_pixel
-      CALL cp_buf2gme(tg,gme_grid,no_raw_data_pixel,gme_int_field)
-      CALL sync_diamond_edge(gme_grid, gme_int_field)
-      CALL cp_gme2buf(tg,gme_grid,gme_int_field,no_raw_data_pixel)
-     END SELECT
-
-
-
-
+    print *,'MAXVAL of NDVI_sum is ', MAXVAL(ndvi_sum)
 
     !HA debug:
     print *,'MAXVAL(no_raw_data_pixel): ',MAXVAL(no_raw_data_pixel)
