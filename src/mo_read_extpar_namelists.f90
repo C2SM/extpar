0370e64f5fb3bf60100d2cae3befb6ed70782b12
Katherine Osterried <b380729@mlogin102.hpc.dkrz.de>
2018-10-19 15:15:27 +0200

    Add merging of fine and coarse T_CL values

--- src/mo_read_extpar_namelists.f90
+++ src/mo_read_extpar_namelists.f90
@@ -270,7 +270,7 @@ CONTAINS
     READ(nuin, NML=extpar_consistency_check_io, IOSTAT=ierr)
 
     CLOSE(nuin)
-
+ 
     IF (lwrite_grib) THEN
       PRINT *,'Grib output is currently not supported!'
       lwrite_grib=.FALSE.
