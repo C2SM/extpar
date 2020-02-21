019aad0ec061f19c547d86b53176ad3aa2cbc1b5
JonasJucker <joni.jucker@yahoo.de>
2020-02-21 15:40:19 +0100

    enable launch of checker in jenkins directly

--- test/testsuite/src/checkers/existence_compiler_warnings.sh
+++ test/testsuite/src/checkers/existence_compiler_warnings.sh
@@ -1,44 +1,24 @@
 #!/bin/bash
 # COSMO TECHNICAL TESTSUITE
 #
-# This script checks whether the Extpar code output is different
-# from the reference using the cdo diffv function.  
+# This script checks whether the compilation of Extpar caused any
+# warnings for the NAG and the GCC compiler
 #
-# Author       Katie Osterried
+# Author       Jonas Jucker 
 # Maintainer   katherine.osterried@env.ethz.ch
 
-# check environment variables
-RUNDIR=${TS_RUNDIR}
-VERBOSE=${TS_VERBOSE}
-REFOUTDIR=${TS_REFOUTDIR}
+# root dir of Extpar repo
 compiledir="../../"
 
 # define compiler warnings to be checked
-compiler_warnings=("Wunused-dummy-argument" \
+compiler_warnings=("Wunused-dummy-argument" \ 
                    "Wconversion" \
                    "Wunused-variable" \
                    "Unused dummy variable" \
                    "Line longer than 132 characters")
 
 
-if [ -z "${VERBOSE}" ] ; then
-  echo "Environment variable TS_VERBOSE is not set" 1>&1
-  exit 20 # FAIL
-fi
-if [ -z "${RUNDIR}" ] ; then
-  echo "Environment variable TS_RUNDIR is not set" 1>&1
-  exit 20 # FAIL
-fi
-if [ -z "${REFOUTDIR}" ] ; then
-  echo "Environment variable TS_REFOUTDIR is not set" 1>&1
-  exit 20 # FAIL
-fi
-if [ ! -d "${RUNDIR}" ] ; then
-  echo "Directory TS_RUNDIR=${RUNDIR} does not exist" 1>&1
-  exit 20 # FAIL
-fi
-
-if [ ! -f "$compiledir/compile.log"* ] ; then
+if [ ! -f "$compiledir/compile.log" ] ; then
   echo "No compilation log-file found"  1>&1
   exit 20 # FAIL
 fi
