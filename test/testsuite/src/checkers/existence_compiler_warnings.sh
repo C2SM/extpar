d69a0316487dbfebe938fe056c50e297c0fae581
JonasJucker <joni.jucker@yahoo.de>
2020-02-25 08:37:53 +0100

    revert warning test

--- test/testsuite/src/checkers/existence_compiler_warnings.sh
+++ test/testsuite/src/checkers/existence_compiler_warnings.sh
@@ -6,25 +6,94 @@
 #
 # Author       Jonas Jucker 
 # Maintainer   katherine.osterried@env.ethz.ch
-
+set -x
 # root dir of Extpar repo
 compiledir="../../"
 
+# warning identifier for different compilers
+warning_gcc="Warning:"
+warning_intel=
+warning_nag=
 # define compiler warnings to be checked
-warnings=("Wunused-dummy-argument" "Wconversion" "Wunused-variable" "Line longer than 132 characters" "Unused dummy variable")
+
+# define compiler warnings to be checked
+to_ignore_gcc=("Wmaybe-uninitialized")
+to_ignore_intel=("Wmaybe-uninitialized")
+to_ignore_nag=("Wmaybe-uninitialized")
 
 if [ ! -f "$compiledir/compile.log" ] ; then
   echo "No compilation log-file found"  1>&1
   exit 20 # FAIL
 fi
 
-for warning in "${warnings[@]}"; do
-    grep "$warning" $compiledir/compile.log > /dev/null
-    if [ $? -ne 1 ] ; then
-       echo "Compiler warnings found for $warning" 1>&1
-       exit 20 # FAIL
+# GCC 
+counter=0
+for ignore in "${to_ignore_gcc[@]}"; do
+    if [[ $counter == "0" ]]; then
+        # look for warnings, but ignore 1 of those defined above
+        grep  "$warning_gcc" $compiledir/compile.log | grep -v "$ignore"  > ignore_step_${counter}
+    else
+
+        # look for warnings, but ignore 1 of those defined above
+        grep "$warning_gcc" ignore_step_$last_iteration | grep -v "$ignore"> ignore_step_${counter}
     fi
+    last_iteration=$counter
+    (( counter++ ))
 done
+
+# look for warnings, if still some present -> exit
+grep "$warning_gcc" ignore_step_$last_iteration > /dev/null
+if [ $? -ne 1 ] ; then
+   echo "Compiler warnings found for GCC" 1>&1
+   exit 20 # FAIL
+fi
+
+# NAG 
+counter=0
+for ignore in "${to_ignore_nag[@]}"; do
+    if [[ $counter == "0" ]]; then
+        # look for warnings, but ignore 1 of those defined above
+        grep  "$warning_nag" $compiledir/compile.log | grep -v "$ignore"  > ignore_step_${counter}
+    else
+
+        # look for warnings, but ignore 1 of those defined above
+        grep "$warning_nag" ignore_step_$last_iteration | grep -v "$ignore"> ignore_step_${counter}
+    fi
+    last_iteration=$counter
+    (( counter++ ))
+done
+
+# look for warnings, if still some present -> exit
+grep "$warning_nag" ignore_step_$last_iteration > /dev/null
+if [ $? -ne 1 ] ; then
+   echo "Compiler warnings found for NAG" 1>&1
+   exit 20 # FAIL
+fi
+
+# INTEL 
+counter=0
+for ignore in "${to_ignore_intel[@]}"; do
+    if [[ $counter == "0" ]]; then
+        # look for warnings, but ignore 1 of those defined above
+        grep  "$warning_intel" $compiledir/compile.log | grep -v "$ignore"  > ignore_step_${counter}
+    else
+
+        # look for warnings, but ignore 1 of those defined above
+        grep "$warning_intel" ignore_step_$last_iteration | grep -v "$ignore"> ignore_step_${counter}
+    fi
+    last_iteration=$counter
+    (( counter++ ))
+done
+
+# look for warnings, if still some present -> exit
+grep "$warning_intel" ignore_step_$last_iteration > /dev/null
+if [ $? -ne 1 ] ; then
+   echo "Compiler warnings found for INTEL" 1>&1
+   exit 20 # FAIL
+fi
+
+#  cleanup
+rm ignore_step_*
 
 # goodbye
 exit 0 # MATCH
