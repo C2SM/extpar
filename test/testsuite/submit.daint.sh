e1e73ccdd48ecab846a429bc4a3859bee0ac833c
Jonas Jucker <joni.jucker@yahoo.de>
2021-03-03 17:52:03 +0100

    disable filelocking on Daint

--- test/testsuite/submit.daint.sh
+++ test/testsuite/submit.daint.sh
@@ -8,6 +8,9 @@ module load CDO
 source ../../modules.env
 source /project/g110/extpar_envs/venv_jenkins_daint/bin/activate
 
+# this is needed to not have problems with the emissivity input files
+export HDF5_USE_FILE_LOCKING=FALSE
+
 
 ./src/testsuite.py --exe=run_extpar_cosmo.sh -v 1 -o testsuite.out --testlist=testlist_cosmo.xml --mpicmd='srun -u -n'  
 ./src/testsuite.py -a --exe=run_extpar_icon.sh -v 1 -o testsuite.out --testlist=testlist_icon.xml --mpicmd='srun -u -n'  
