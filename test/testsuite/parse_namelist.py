d72b20fa04ab1f0dc8b213a117704523a08f82ca
juckerj <jonas.jucker@env.ethz.ch>
2021-08-06 18:33:40 +0200

    save commit

--- test/testsuite/parse_namelist.py
+++ test/testsuite/parse_namelist.py
@@ -1,48 +1,78 @@
 
 import glob
-namelists = glob.glob('INPUT_*')
-#namelists = ['INPUT_ORO','INPUT_AOT']
-variable = 'test'
-type_to_convert = str
-clean_files = []
-for namelist in namelists:
-    with open(namelist, 'r') as f: 
-        # read line by line
-        for line in f:
-            line = line.rstrip().lstrip() 
+import os
 
-            # line is commented
-            if line.startswith("!"):
-                logging.warning('Ignore commented line: '
-                                f'{line}')
+def filter_string_list(string_list, filter_strings):
+    '''
+    Keep all entries of a list of strings, that do not
+    contain an entry of filter_strings
+    '''
+    filtered_string = []
+    for line in string_list:
+        if not any(filter_string in line.lower() for filter_string in filter_strings):
+            filtered_string.append(line)
 
-            # valid entry in namelist
-            else:
+    return filtered_string
 
-                if ".nc" in line:
-                    split = line.split('=')
+testlist = glob.glob('data/*/*')
 
-                    # return last element of split 
-                    raw_variable = split[-1].strip()
+datafiles_raw = []
+datafiles_no_duplicates = []
 
-                    characters_to_strip = ["'", ",", '"']
-                    for character in characters_to_strip:
-                        raw_variable = raw_variable.strip(character)
+print('Extract data files contained in namelists:')
+for test in testlist:
+    if 'intel' not in test:
+        print('  - {}'.format(test))
 
+        namelists_per_test = glob.glob(test + '/INPUT_*')
 
-                    if " " in raw_variable:
-                        split = raw_variable.split(" ")
-                        characters_to_strip = ["'", ",", '"']
-                        for file in split:
-                            for character in characters_to_strip:
-                                file = file.strip(character)
-                            if len(file) != 0:
-                                    clean_files.append(file)
-                    else:
-                        if not "BUFFER" in raw_variable.upper():
-                            if not "ICON" in raw_variable.upper():
-                                clean_files.append(str(raw_variable))
+        for namelist in namelists_per_test:
+            if 'INPUT_CHECK' not in namelist:
 
-for file in clean_files:
+                with open(namelist, 'r') as f: 
+
+                    # read line by line
+                    for line in f:
+                        line = line.rstrip().lstrip() 
+
+                        # line is commented
+                        if line.startswith("!"):
+                            print('*** Ignore commented line: '
+                                            f'{line}')
+
+                        # valid entry in namelist
+                        else:
+
+                            if ".nc" in line:
+                                split = line.split('=')
+
+                                # return last element of split 
+                                raw_variable = split[-1].strip()
+
+                                characters_to_strip = ["'", ",", '"']
+                                for character in characters_to_strip:
+                                    raw_variable = raw_variable.strip(character)
+
+                                # for entries with more than one data file i.e. Aster tiles
+                                if " " in raw_variable:
+                                    tile_data = raw_variable.split(" ")
+                                    characters_to_strip = ["'", ",", '"']
+                                    for tile in tile_data:
+                                        for character in characters_to_strip:
+                                            tile = tile.strip(character)
+                                        if len(tile) != 0:
+                                                datafiles_raw.append(tile.strip("'"))
+                                else:
+                                    datafiles_raw.append(str(raw_variable.rstrip("'")))
+
+print(len(datafiles_raw))
+datafiles_no_duplicates = list(dict.fromkeys(datafiles_raw))
+print(len(datafiles_no_duplicates))
+
+bad_words = ['buffer','icon','external','@','corine']
+datafiles_clean = filter_string_list(datafiles_no_duplicates, bad_words)
+print(len(datafiles_clean))
+
+for file in datafiles_clean:
     print(file)
 
