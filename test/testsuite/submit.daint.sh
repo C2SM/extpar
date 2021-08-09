71d17d2f96e0814dadf40a54010bbb8b7b6b1d46
Jonas Jucker <joni.jucker@yahoo.de>
2021-08-09 18:11:03 +0200

    enable openmp at CSCS

--- test/testsuite/submit.daint.sh
+++ test/testsuite/submit.daint.sh
@@ -3,6 +3,7 @@
 #SBATCH --output="job.out"
 #SBATCH --account=g110
 #SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=12
 module load daint-gpu
 module load CDO
 source ../../modules.env
@@ -10,6 +11,8 @@ source /project/g110/extpar_envs/venv_jenkins_daint/bin/activate
 
 # this is needed to not have problems with the emissivity input files
 export HDF5_USE_FILE_LOCKING=FALSE
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 
 ./src/testsuite.py --exe=run_extpar_cosmo.sh -v 1 -o testsuite.out --testlist=testlist_cosmo.xml --mpicmd='srun -u -n'  
