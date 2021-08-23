253d4a71b83d9bcac9c919de64abbd34e0758495
Jonas Jucker <joni.jucker@yahoo.de>
2021-08-23 17:04:37 +0200

    Bilinear interpolation for ahf/isa

--- python/extpar_isa_to_buffer.py
+++ python/extpar_isa_to_buffer.py
@@ -113,7 +113,7 @@ logging.info('============= CDO: remap to target grid ========')
 logging.info('')
 
 # calculate weights
-utils.launch_shell('cdo', '-f', 'nc4', '-L', '-P', omp,  f'genycon,{grid}',
+utils.launch_shell('cdo', '-f', 'nc4', '-L', '-P', omp,  f'genbil,{grid}',
                    tg.cdo_sellonlat(),
                    raw_data_isa, weights)
 
