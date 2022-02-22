245d07647954e9e96a810d7517b5eb0ed90575c7
Jonas Jucker <joni.jucker@yahoo.de>
2022-02-22 09:40:01 +0100

    increase time

--- test/testsuite/submit.daint.sh
+++ test/testsuite/submit.daint.sh
@@ -2,7 +2,7 @@
 #SBATCH --constraint=gpu
 #SBATCH --output="job.out"
 #SBATCH --account=g110
-#SBATCH --time=00:20:00
+#SBATCH --time=02:00:00
 #SBATCH --cpus-per-task=12
 module load daint-gpu
 module load CDO
