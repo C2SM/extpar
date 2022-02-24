2d3058a4b09a6ef8b897a471860a23fc10df7f33
juckerj <39263956+jonasjucker@users.noreply.github.com>
2022-02-24 08:26:06 +0100

    [Daint] Pmi fix (#281)
    * temporary fix for Daint
    
    * remove test icon_d2_caching from Daint
    
    * increase time
    
    * fix for pmi
    
    * use correct venv on Tsa

--- test/testsuite/submit.daint.sh
+++ test/testsuite/submit.daint.sh
@@ -12,6 +12,9 @@ source /project/g110/extpar/venv_daint/bin/activate
 # this is needed to not have problems with the emissivity input files
 export HDF5_USE_FILE_LOCKING=FALSE
 
+# avoid messages for CDO calls when submitted from login node
+export PMI_NO_PREINITIALIZE=1
+
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 
