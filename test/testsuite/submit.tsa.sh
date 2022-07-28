f397971f486d60ea8f63fe4769be45ed482f7c1b
Stephanie Westerhuis <stephanie.westerhuis@usys.ethz.ch>
2022-07-28 13:39:43 +0200

    removed osm and opr-paths

--- test/testsuite/submit.tsa.sh
+++ test/testsuite/submit.tsa.sh
@@ -6,11 +6,8 @@
 #SBATCH --partition=postproc
 #SBATCH --cpus-per-task=12
 
-source /oprusers/osm/.opr_setup_dir
-export MODULEPATH=$MODULEPATH\:$OPR_SETUP_DIR/modules/modulefiles
 source /project/g110/extpar/venv_tsa/bin/activate
 source ../../modules.env
-module load cdo
 export HDF5_DISABLE_VERSION_CHECK=1
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
