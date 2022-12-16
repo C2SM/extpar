ac76c1b8150a9cee65e077a57c23b19ccb57290c
Philipp Sommer <philipp.sommer@hereon.de>
2022-12-16 15:54:40 +0100

    implement setup script and versioneer

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup script for the extpar python bindings.
+"""
+
+import versioneer
+from setuptools import setup
+
+setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
