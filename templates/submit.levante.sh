434946a763b143a9ff8dd58620d64d83db0d5c17
Philipp S. Sommer <philipp.sommer@hereon.de>
2023-01-26 12:59:02 +0100

    fix runscript permissions (#331)
    * source /etc/profile if not yet done
    
    * do not make runscript writeable for everyone

--- templates/submit.levante.sh
+++ templates/submit.levante.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="extpar"
 #SBATCH --mem=50G
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=12 
+#SBATCH --cpus-per-task=12
 #SBATCH --output="job.out"
 #SBATCH --time=02:00:00
 #SBATCH --partition=shared,compute
@@ -13,6 +13,9 @@ export PYTHONPATH=@PYTHONPATH@:$PYTHONPATH
 
 logfile=extpar.log
 rm $logfile
+
+# source /etc/profile if this has not yet been done
+if ! command -v module &> /dev/null; then source /etc/profile; fi
 
 if [ -e modules.env ]; then source modules.env; fi
 source runcontrol_functions.sh
