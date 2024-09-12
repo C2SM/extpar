a4b72ab2e3ba7a4d0531cc0002120993cb828f33
Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
2024-09-12 10:40:22 +0200

    add Balfrin

--- test/jenkins/build.sh
+++ test/jenkins/build.sh
@@ -18,21 +18,16 @@ function run_command {
 
 case "$(hostname)" in
     # CSCS machines
-    daint*)
-        run_command git submodule init
-        run_command git submodule update
-        run_command ./configure.daint.gcc
+    balfrin*)
+        run_command ./configure.balfrin.gcc
         run_command source modules.env
-        run_command make clean
         echo compile extpar...
         run_command make &> compile.log
         echo          ...done
         echo See compile.log for more information!
 
         if [[ $compiler == 'python-package' ]]; then
-            run_command module load cray-python
-            run_command python -m venv venv
-            . venv/bin/activate
+            run_command source modules.env
             run_command python setup.py sdist
             run_command pip install dist/extpar-*.tar.gz
             run_command python -m extpar.WrapExtpar -h
@@ -41,30 +36,12 @@ case "$(hostname)" in
 
         ;;
 
-    tsa*)
-        run_command git submodule init
-        run_command git submodule update
-        run_command ./configure.tsa.gcc
-        run_command source modules.env
-        run_command make clean
-        echo compile extpar...
-        run_command make &> compile.log
-        echo          ...done
-        echo See compile.log for more information!
-        if [[ $compiler == 'python-package' ]]; then
-            echo 'python-package not built on Tsa'
-            exit 1
-        fi
-        ;;
-
     # DKRZ machines    
     *levante*)
         if [[ -r /sw/etc/profile.levante ]]
         then
            source /sw/etc/profile.levante
         fi
-        run_command git submodule init
-        run_command git submodule update
         run_command ./configure.levante.gcc
         run_command source modules.env
         run_command make clean
