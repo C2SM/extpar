d97a040b1bcace31ec58c616fba43d55e2a59aa7
Jonas Jucker <jonas.jucker@c2sm.ethz.ch>
2025-03-26 15:50:45 +0100

    grep for failure

--- test/jenkins/test_docker.sh
+++ test/jenkins/test_docker.sh
@@ -8,4 +8,11 @@ cd ..
 # Copy the executables
 cp ../../bin/* bin
 
-./submit.docker.sh
+./submit.docker.sh 2>&1 | tee -a testsuite.out
+
+grep RESULT testsuite.out | egrep 'FAIL|CRASH' > /dev/null
+if [ $? -eq 0 ] ; then
+   echo "testsuite did not complete successfully"
+   exit 1
+
+fi
