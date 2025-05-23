b65b1c3391c9a92f5cb66624b9e3dbaa58158382
Mikael Stellio <mikael.stellio@c2sm.ethz.ch>
2025-05-23 10:46:00 +0200

    Check if tests fail when no argument is provided

--- python/WrapExtpar.py
+++ python/WrapExtpar.py
@@ -841,6 +841,7 @@ def replace_placeholders(args, templates, dir, actual_values):
             raise ValueError(
                 f'Not all placeholders in {all_templates[template]} were replaced'
             )
+            # Add check for None here
 
     # write complete template to file
     for template in templates:
