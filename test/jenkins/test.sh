c482266c6f243ffdb3689998a47a595d6e1ca093
Mikael Stellio <mikael.stellio@c2sm.ethz.ch>
2025-07-16 17:39:41 +0200

    Added support to pass testsuite verbosity from Jenkins

--- test/jenkins/test.sh
+++ test/jenkins/test.sh
@@ -2,7 +2,7 @@
 case "$(hostname)" in
     *co2* | *iacdipl-7*)
         set -e
-        podman run -e OMP_NUM_THREADS=16 -v /net/co2/c2sm-data/extpar-input-data:/data extpar:$ghprbPullId bash -c /workspace/test/jenkins/test_docker.sh || (podman image rm -f extpar:$ghprbPullId && exit 1)
+        podman run -e OMP_NUM_THREADS=16 -v /net/co2/c2sm-data/extpar-input-data:/data extpar:$ghprbPullId bash -c /workspace/test/jenkins/test_docker.sh ${1:-"2"} || (podman image rm -f extpar:$ghprbPullId && exit 1)
         podman image rm -f extpar:$ghprbPullId
         exit 0
         ;;
--- test/jenkins/test_docker.sh
+++ test/jenkins/test_docker.sh
@@ -8,11 +8,10 @@ cd ..
 # Copy the executables
 cp ../../bin/* bin
 
-./submit.docker.sh 2>&1 | tee -a testsuite.out
+./submit.docker.sh ${1:-"2"} 2>&1 | tee -a testsuite.out
 
 grep RESULT testsuite.out | egrep 'FAIL|CRASH' > /dev/null
 if [ $? -eq 0 ] ; then
    echo "testsuite did not complete successfully"
    exit 1
-
 fi
