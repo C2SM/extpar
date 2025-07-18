9d1596f0900464936ec5ab9d2f2cafd4d01e288d
stelliom <67868694+stelliom@users.noreply.github.com>
2025-07-18 13:40:45 +0200

    Update test_docker.sh

--- test/jenkins/test_docker.sh
+++ test/jenkins/test_docker.sh
@@ -10,6 +10,15 @@ cp ../../bin/* bin
 
 ./submit.docker.sh ${1:-"2"} 2>&1 | tee -a testsuite.out
 
+cp testsuite.out /net/co2/c2sm-services/extpar/test/.
+for dir in work/*/; do
+   subdir=$(basename $dir)
+   mkdir -p /net/co2/c2sm-services/extpar/test/$subdir
+   cp $dir/*.log /net/co2/c2sm-services/extpar/test/$subdir/.
+   cp $dir/INPUT_* /net/co2/c2sm-services/extpar/test/$subdir/.
+   cp $dir/TEST_RES /net/co2/c2sm-services/extpar/test/$subdir/.
+done
+
 grep RESULT testsuite.out | egrep 'FAIL|CRASH' > /dev/null
 if [ $? -eq 0 ] ; then
    echo "testsuite did not complete successfully"
