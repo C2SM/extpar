80689df76fc4e65d1aa111c5d15931a13dfa99f0
Mikael Stellio <mikael.stellio@c2sm.ethz.ch>
2025-07-18 17:00:50 +0200

    Added MODE argument

--- test/jenkins/test.sh
+++ test/jenkins/test.sh
@@ -3,11 +3,17 @@ case "$(hostname)" in
     *co2* | *iacdipl-7*)
         set -e
 
-        current_time=$(date --iso-8601=seconds)
-        hash=$(echo -n "$current_time" | sha256sum | awk '{print $1}')
-        mkdir -p /net/co2/c2sm-services/extpar/$hash
+        MODE=${1:-'NORMAL'}
+        if [[ ${MODE} == "DEBUG" ]]; then
+            CURRENT_DATETIME=$(date --iso-8601=seconds)
+            HASH=$(echo -n "${CURRENT_DATETIME}" | sha256sum | awk '{print $1}')
+            mkdir -p /net/co2/c2sm-services/extpar/${HASH}
 
-        podman run -e OMP_NUM_THREADS=16 -v /net/co2/c2sm-data/extpar-input-data:/data -v /net/co2/c2sm-services/extpar/$hash:/artifacts extpar:$ghprbPullId bash -c "/workspace/test/jenkins/test_docker.sh ${1:-'2'}" || (podman image rm -f extpar:$ghprbPullId && exit 1)
+            podman run -e OMP_NUM_THREADS=16 -v /net/co2/c2sm-data/extpar-input-data:/data -v /net/co2/c2sm-services/extpar/${HASH}:/artifacts extpar:$ghprbPullId bash -c "/workspace/test/jenkins/test_docker.sh ${MODE}" || (podman image rm -f extpar:$ghprbPullId && exit 1)
+        else
+            podman run -e OMP_NUM_THREADS=16 -v /net/co2/c2sm-data/extpar-input-data:/data extpar:$ghprbPullId bash -c "/workspace/test/jenkins/test_docker.sh ${MODE}" || (podman image rm -f extpar:$ghprbPullId && exit 1)
+        fi
+
         podman image rm -f extpar:$ghprbPullId
         exit 0
         ;;
--- test/testsuite/submit.docker.sh
+++ test/testsuite/submit.docker.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-VERBOSITY=${1:-"2"}
-
-./src/testsuite.py -a --exe=run_extpar_cosmo.sh -v ${VERBOSITY} --testlist=testlist_cosmo.xml --mpicmd='sleep 1 &&'
-./src/testsuite.py -a --exe=run_extpar_icon.sh -v ${VERBOSITY} --testlist=testlist_icon.xml --mpicmd='sleep 1 &&'
-./src/testsuite.py -a --exe=run_extpar_icon.sh -v ${VERBOSITY} --testlist=testlist_landuse.xml --mpicmd='sleep 1 &&'
+./src/testsuite.py -a --exe=run_extpar_cosmo.sh -v 2 --testlist=testlist_cosmo.xml --mpicmd='sleep 1 &&'
+./src/testsuite.py -a --exe=run_extpar_icon.sh -v 2 --testlist=testlist_icon.xml --mpicmd='sleep 1 &&'
+./src/testsuite.py -a --exe=run_extpar_icon.sh -v 2 --testlist=testlist_landuse.xml --mpicmd='sleep 1 &&'
