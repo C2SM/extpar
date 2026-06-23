0fca7824fe779320bdb9a776839b2ee4e64c5e9d
Mikael Stellio <mikael.stellio@c2sm.ethz.ch>
2026-06-23 15:08:57 +0200

    Use R02B04 global grid

--- test/testsuite/data/get_data.sh
+++ test/testsuite/data/get_data.sh
@@ -49,7 +49,7 @@ wget --quiet 'ftp://iacftp.ethz.ch/pub_read/stelliom/external_parameter_icon_ecc
 cd -
 
 cd dwd/icon_global
-wget --quiet 'http://icon-downloads.mpimet.mpg.de/grids/public/edzw/icon_grid_0016_R02B06_G.nc'
+wget --quiet 'http://icon-downloads.mpimet.mpg.de/grids/public/edzw/icon_grid_0012_R02B04_G.nc'
 cd -
 
 # mpim
