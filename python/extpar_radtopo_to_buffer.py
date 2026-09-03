65011d69be701fc3f51dba7d172ef40c5f8cecfb
Christian Steger <csteger@balfrin-ln002.cscs.ch>
2026-09-03 14:32:38 +0200

    First part of Python subgrid-scale implementation completed

--- python/extpar_radtopo_to_buffer.py
+++ python/extpar_radtopo_to_buffer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import logging
+from time import perf_counter
 import sys
 import numpy as np
 import xarray as xr
@@ -61,6 +62,11 @@ elif (igrid_type != 1):
     error_message = "RADTOPO (HORAYZON) only works with ICON"
     logging.error(error_message)
     raise ValueError(error_message)
+
+topo_data_set = {1: "GLOBE", 2: "ASTER", 3: "MERIT", 4: "COPERNICUS"}
+
+# Constants
+radius_earth = 6_371_229.0  # ICON/COSMO earth radius [m]
 
 #--------------------------------------------------------------------------
 # Compute radiation-topography parameters on grid-scale
@@ -129,7 +135,6 @@ if iradtopo["radtopo_type"] == 2:
     earth_centre = np.array(
         [0.0, 0.0, 0.0], dtype=np.float64).reshape((1, 3))
     radtopo.ecef2enu(earth_centre, lon_origin, lat_origin)
-    radius_earth = 6_371_229.0
     north_pole = np.array(
         [0.0, 0.0, radius_earth], dtype=np.float64).reshape((1, 3))
     radtopo.ecef2enu(north_pole, lon_origin, lat_origin)
@@ -198,7 +203,6 @@ if iradtopo["radtopo_type"] == 2:
     logging.info('')
     logging.info('============= write to buffer file =============')
     logging.info('')
-    topo_data_set = {1: "GLOBE", 2: "ASTER", 3: "MERIT", 4: "COPERNICUS"}
     ie_tot = clon.size
     lon = np.rad2deg(clon)
     lat = np.rad2deg(clat)
@@ -228,10 +232,251 @@ else:
     logging.info('=== compute subgrid-scale radtopo parameters ===')
     logging.info('')
 
-    # Get relevant raw topography files -> when ASTER is selected,
-    # always use Copernicus DEM !!!
+    # Check input topography
+    itopo_type = ioro["itopo_type"]
+    if itopo_type == 1:
+        sys.exit("GLOBE not supported for subgrid-scale radtopo")
+    elif itopo_type == 2:
+        logging.info(
+            'ASTER not supported for subgrid-scale radtopo due to artefacts\n.'
+            'Copernicus DEM is used instead (only for subgrid-scale radtopo)'
+        )
+        itopo_type = 4
 
-    logging.info('Not yet implemented')
+    # Input DEM settings
+    if itopo_type == 3: # MERIT
+        dem_spacing = 3.0 / 3600.0  # [deg]
+        num_tile_lon, num_tile_lat = 12, 6
+    else: # COPERNICUS
+        dem_spacing = 1.0 / 3600.0  # [deg]
+        num_tile_lon, num_tile_lat = 18, 18
+
+    # Resolution of DEM
+    earth_circ = 40_075_000.0  # equatorial circumference of Earth [m]
+    dem_spacing_m = (earth_circ / 360.0) * dem_spacing  # ~max. spacing [m]
+    logging.info(f"DEM resolution: {(dem_spacing_m):.1f} m")
+    cell_area_dem = dem_spacing_m ** 2  # [m2]
+
+    # Load ICON grid with specific resolution
+    with xr.open_dataset(icon_grid) as ds:
+        vlon = ds["vlon"].values
+        vlat = ds["vlat"].values
+        vertex_of_cell = ds["vertex_of_cell"].values - 1  # (3, num_cell)
+        # ordered counter-clockwise
+        edge_of_cell = ds["edge_of_cell"].values - 1  # (3, num_cell)
+        # ordered counter-clockwise
+        edge_vertices = ds["edge_vertices"].values - 1  # (2, num_edge)
+        grid_level = int(ds.attrs["grid_level"])
+        grid_root = int(ds.attrs["grid_root"])
+    res_icon = (5050.0 / (grid_root * 2 ** grid_level)) * 1000.0  # [m]
+    logging.info(f"ICON resolution: {res_icon:.1f} m")
+    cell_area_icon = res_icon ** 2  # [m2]
+
+    # Cartesian coordinates (unit sphere)
+    vertices = np.column_stack((
+        np.cos(vlat) * np.cos(vlon),
+        np.cos(vlat) * np.sin(vlon),
+        np.sin(vlat)
+    ))
+
+    # Compute refinement level
+    logging.info(" Computed refinement level ".center(60, "-"))
+    # n ** 2 = cell_area_icon / cell_area_dem -> solve for n
+    n_sel = np.sqrt(cell_area_icon / cell_area_dem)
+    n_sel = round(n_sel)  # closest to DEM resolution
+    logging.info(f"Division steps (n): {n_sel}")
+    res_icon_fine = np.sqrt(cell_area_icon / (n_sel ** 2))
+    logging.info(f"Refined mesh resolution: {res_icon_fine:.1f} m")
+    num_tri_fine = vertex_of_cell.shape[1] * (n_sel ** 2)
+    logging.info(
+        f"Number of resulting triangles: {num_tri_fine:,}".replace(",", "'")
+    )
+    logging.info(
+        f"Size of 'faces_child' array: {(12 * num_tri_fine / 1e9):.2f} GB"
+    )
+    logging.info("-" * 60)
+
+    # Refine ICON triangle mesh
+    vertices_child, faces_child = radtopo.refine_tri_mesh(
+        vertices,
+        vertex_of_cell,
+        edge_of_cell,
+        edge_vertices,
+        n_sel,
+    )
+
+    # Check size of vertices array
+    size_float32 = vertices_child.nbytes / (2.0 * 1e9) # [GB]
+    logging.info(
+        f"Size of 'vertices_child' array: {size_float32:.2f} GB (32-bit float)"
+    )
+    if size_float32 > 16.0:
+        raise ValueError("Triangle vertices array is larger than 16 GB")
+
+    # Values relevant for child-parent triangle relation
+    num_cell_parent = vertex_of_cell.shape[1]
+    num_cell_child_per_parent = n_sel ** 2
+
+    # Compute spherical coordinates (longitude/latitude) of child vertices
+    t_beg = perf_counter()
+    np.arctan2(vertices_child[:, 1], vertices_child[:, 0],
+            out=vertices_child[:, 0])
+    np.arcsin(vertices_child[:, 2], out=vertices_child[:, 1])
+    vertices_child[:, 2] = np.nan  # defined later...
+    t_end = perf_counter()
+    logging.info(f"Coordinate transformation: {t_end - t_beg:.1f} s")
+
+    # Interpolate elevation data from raw DEM on refined mesh vertices
+    logging.info(f"Input DEM: {topo_data_set[itopo_type]}")
+    idx_lin_tile = radtopo.assign_points_to_tiles(
+        vertices_child[:, 0],
+        vertices_child[:, 1],
+        num_tile_lon,
+        num_tile_lat,
+    )
+    elevation_interp = np.empty(idx_lin_tile.size, dtype=np.float32)
+    num_tile = (num_tile_lon, num_tile_lat)
+    for idx_lin in np.unique(idx_lin_tile):
+
+        # Get relevant points for tile
+        mask = (idx_lin_tile == idx_lin)
+        lon_vert_tile = vertices_child[mask, 0]
+        lat_vert_tile = vertices_child[mask, 1]
+        # print(np.rad2deg(lon_vert_tile.min()), np.rad2deg(lon_vert_tile.max()))
+        # print(np.rad2deg(lat_vert_tile.min()), np.rad2deg(lat_vert_tile.max()))
+
+        # Bounding box for DEM domain
+        lon_min = np.rad2deg(lon_vert_tile.min()) - dem_spacing
+        lon_max = np.rad2deg(lon_vert_tile.max()) + dem_spacing
+        lat_min = np.rad2deg(lat_vert_tile.min()) - dem_spacing
+        lat_max = np.rad2deg(lat_vert_tile.max()) + dem_spacing
+
+        # Longitude (i) and latitude (j) tile index
+        i = idx_lin % num_tile_lon
+        j = idx_lin // num_tile_lon
+        # print(i, j)
+
+        # Get a list of required DEM tiles
+        i_left = (i + 1) % num_tile_lon
+        dem_tiles = [
+            radtopo.get_tile_name(i, j, *num_tile),
+            radtopo.get_tile_name(i_left, j, *num_tile),
+        ]
+        if itopo_type == 4:  # COPERNICUS
+            if j + 1 < 18:
+                j_below = j + 1
+                dem_tiles.append(
+                    radtopo.get_tile_name(i, j_below, *num_tile)
+                )
+                dem_tiles.append(
+                    radtopo.get_tile_name(i_left, j_below, *num_tile)
+                )
+            lon_slice = slice(0, 3_600 * 20 + 1)
+            lat_slice = slice(0, 3_600 * 10 + 1)
+            dem_tiles = [f"COPERNICUS_{tile}.nc" for tile in dem_tiles]
+            var_elevation = "elevation"
+        else: # MERIT
+            if j - 1 >= 0:
+                j_above = j - 1
+                dem_tiles.append(
+                    radtopo.get_tile_name(i, j_above, *num_tile)
+                )
+                dem_tiles.append(
+                    radtopo.get_tile_name(i_left, j_above, *num_tile)
+                )
+            lon_slice = slice(0, 1_200 * 30 + 1)
+            lat_slice = slice(1_200 * 30 - 1, 2 * 1_200 * 30)
+            dem_tiles = [f"MERIT_{tile}.nc" if tile[:3] != "S60"
+                         else f"REMA_BKG_{tile}.nc" for tile in dem_tiles]
+            var_elevation = "Elevation"
+        logging.info("\n".join(dem_tiles))
+
+        # Load DEM data
+        with xr.open_mfdataset(
+            [tile for tile in dem_tiles], mask_and_scale=False
+        ) as ds:
+            ds = ds.isel(lon=lon_slice, lat=lat_slice)
+            # print(ds["lon"].values[[0, -1]], ds["lat"].values[[0, -1]])
+            ds = ds.sel(
+                lon=slice(lon_min, lon_max),
+                lat=slice(lat_max, lat_min)
+            )
+            # print(ds["lon"].values[[0, -1]], ds["lat"].values[[0, -1]])
+            lon_dem = np.deg2rad(ds["lon"].values)  # [rad]
+            lat_dem = np.deg2rad(ds["lat"].values)  # [rad]
+            elevation_dem = ds[var_elevation].values  # [m]
+            if itopo_type == 3:  # MERIT
+                elevation_dem[elevation_dem == -32767] = 0
+            elevation_dem = elevation_dem.astype(np.float32)
+        logging.info(f"Elevation range: {elevation_dem.min():.1f} "
+                     f"– {elevation_dem.max():.1f} m")
+
+        # Interpolate elevation bilinearly from DEM to triangle mesh vertices
+        if ((lon_vert_tile.min() < lon_dem.min())
+            or (lon_vert_tile.max() > lon_dem.max())
+            or (lat_vert_tile.min() < lat_dem.min())
+            or (lat_vert_tile.max() > lat_dem.max())):
+            raise ValueError("Interpolation point(s) outside of source grid")
+            # -> this check has to be removed for the North/South Pole because
+            #    there, extrapolation is required (-> clamping to grid)
+        atol_grid = np.deg2rad(1.0e-8)  # ca. 1 mm (max) for degree
+        elevation_interp[mask] = radtopo.interp_bilinear(
+            elevation_dem,
+            lon_dem,
+            lat_dem,
+            lon_vert_tile,
+            lat_vert_tile,
+            atol_grid,
+        )
+    vertices_child[:, 2] = elevation_interp
+    del lon_dem, lat_dem, elevation_dem
+    del lon_vert_tile, lat_vert_tile, elevation_interp
+
+    # Coordinate transformation (lon/lat to ENU)
+    radtopo.lonlat2ecef(vertices_child)
+    coord_origin = vertices_child.mean(axis=0)
+    radius = np.sqrt((coord_origin ** 2).sum())
+    lon_origin = np.arctan2(coord_origin[1], coord_origin[0])  # y / x
+    lat_origin = np.arcsin(coord_origin[2] / radius)  # z / radius
+    # works correctly for ICON domains containing the North/South Pole and/or
+    # crossing the +/- 180 deg meridian
+    print(f"Origin of ENU system: "
+        f"lon = {np.rad2deg(lon_origin):.3f} deg, "
+        f"lat = {np.rad2deg(lat_origin):.3f} deg")
+    radtopo.ecef2enu(
+        vertices_child, lon_origin=lon_origin, lat_origin=lat_origin
+    )
+
+    # Compute the earth centre and North Pole in ENU coordinates
+    earth_centre = np.array(
+        [0.0, 0.0, 0.0], dtype=np.float64).reshape((1, 3))
+    radtopo.ecef2enu(
+        earth_centre, lon_origin=lon_origin, lat_origin=lat_origin
+    )
+    north_pole = np.array(
+        [0.0, 0.0, radius_earth], dtype=np.float64).reshape((1, 3))
+    radtopo.ecef2enu(north_pole, lon_origin=lon_origin, lat_origin=lat_origin)
+
+    # Type casting to float32 for Embree
+    vertices_child = vertices_child.astype(np.float32)
+    earth_centre = earth_centre[0, :].astype(np.float32)
+    north_pole = north_pole[0, :].astype(np.float32)
+
+    # Build Embree BVH from terrain triangle mesh
+    terrain = hray.Terrain()
+    terrain.initialise(
+        vertices_child,
+        faces_child,
+        earth_centre,
+        north_pole,
+        )
+
+
+
+
+    logging.info('')
+    logging.info('============= write to buffer file =============')
+    logging.info('')
 
 logging.info('')
 logging.info('============= extpar_radtopo_to_buffer done =======')
